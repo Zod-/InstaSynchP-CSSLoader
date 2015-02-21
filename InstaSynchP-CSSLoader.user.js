8ec4a6a5973c0d316f0cf1186d07e2671274338c
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-02-21 12:10:35 +0100

    Formatting

--- InstaSynchP-CSSLoader.user.js
+++ InstaSynchP-CSSLoader.user.js
@@ -17,58 +17,58 @@
 // ==/UserScript==
 
 function CSSLoader(version) {
-    "use strict";
-    this.version = version;
-    this.name = 'InstaSynchP CSSLoader';
-    this.styles = {};
+  "use strict";
+  this.version = version;
+  this.name = 'InstaSynchP CSSLoader';
+  this.styles = {};
 }
 
 CSSLoader.prototype.executeOnceCore = function () {
-    "use strict";
-    var th = this;
-    window.cssLoader = (function () {
+  "use strict";
+  var th = this;
+  window.cssLoader = (function () {
 
-        return {
-            'add': function (style) {
-                //set the id as the name if it didn't get set
-                if (!style.id) {
-                    style.id = style.name;
-                }
+    return {
+      'add': function (style) {
+        //set the id as the name if it didn't get set
+        if (!style.id) {
+          style.id = style.name;
+        }
 
-                //save the style
-                th.styles[style.name] = style;
+        //save the style
+        th.styles[style.name] = style;
 
-                //load it
-                if (style.autoload) {
-                    cssLoader.load(style.name);
-                }
-            },
-            'load': function (styleName) {
-                var style = th.styles[styleName],
-                    id = '#{0}'.format(style.id);
-                $(id).remove();
+        //load it
+        if (style.autoload) {
+          cssLoader.load(style.name);
+        }
+      },
+      'load': function (styleName) {
+        var style = th.styles[styleName],
+          id = '#{0}'.format(style.id);
+        $(id).remove();
 
-                $('head').append(
-                    $('<link>', {
-                        'rel': 'stylesheet',
-                        'type': 'text/css',
-                        'id': style.id,
-                        'href': style.url
-                    }).on('load', function () {
-                        var ref = this;
-                        //fire event after the CSS has been loaded
-                        setTimeout(function () {
-                            events.fire('CSSLoad[{0}]'.format($(ref).attr('id')));
-                        }, 1000);
-                    })
-                );
-                //if the is nothing to load fire the event directly
-                if (style.url === '') {
-                    events.fire('CSSLoad[{0}]'.format(style.id));
-                }
-            }
-        };
-    }());
+        $('head').append(
+          $('<link>', {
+            'rel': 'stylesheet',
+            'type': 'text/css',
+            'id': style.id,
+            'href': style.url
+          }).on('load', function () {
+            var ref = this;
+            //fire event after the CSS has been loaded
+            setTimeout(function () {
+              events.fire('CSSLoad[{0}]'.format($(ref).attr('id')));
+            }, 1000);
+          })
+        );
+        //if the is nothing to load fire the event directly
+        if (style.url === '') {
+          events.fire('CSSLoad[{0}]'.format(style.id));
+        }
+      }
+    };
+  }());
 };
 
 window.plugins = window.plugins || {};
